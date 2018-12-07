d6c06d6e824f78254fa2c61a7c4e391ad40b13b7
Sagar <gupta-sagar@outlook.com>
2018-12-07 15:11:15 +0530

    Added CustomUser Model

--- django_socionet/apps/users/models.py
+++ django_socionet/apps/users/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+    """
+    Custom User Model for storing extra profile 
+    information.
+    """
+    dob = models.DateField(null = True)
