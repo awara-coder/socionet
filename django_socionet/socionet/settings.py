a607e79228a1e49fffe3290071b9c6096a35ab21
Sagar <gupta-sagar@outlook.com>
2018-12-07 14:54:57 +0530

    Added CustomUser as default user

--- django_socionet/socionet/settings.py
+++ django_socionet/socionet/settings.py
@@ -122,3 +122,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Added CustomUser as default user for Auth app
+AUTH_USER_MODEL = 'users.CustomUser'
