a673b06a896a13f31a1f72c1a59ed4ed82ddfa80
Sagar <gupta-sagar@outlook.com>
2018-12-08 20:28:43 +0530

    Added CustomUser to admin.py

--- django_socionet/apps/users/admin.py
+++ django_socionet/apps/users/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .forms import CustomUserChangeForm, CustomUserCreationForm
+from .models import CustomUser
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    list_display = ['email', 'username', 'dob']
+    model = CustomUser
+
+admin.site.register(CustomUser, CustomUserAdmin)     
